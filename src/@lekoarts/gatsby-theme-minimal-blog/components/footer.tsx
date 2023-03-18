f489e89d7039fad0d9e87d815f0e5f6fdc1b56ae
Spencer Dean <srdean@wesleyan.edu>
2023-03-17 21:36:09 -0700

    footer github link

--- src/@lekoarts/gatsby-theme-minimal-blog/components/footer.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/footer.tsx
@@ -23,6 +23,15 @@ const Footer = () => {
       <div>
         &copy; {new Date().getFullYear()} by Spencer Dean.
       </div>
+      <div>
+        <Link
+          aria-label="Link to the theme's GitHub repository"
+          sx={{ ml: [0, 0, 3] }}
+          href="https://github.com/spencerrdean/spencedean-2022"
+        >
+          View source code on GitHub.
+        </Link>
+      </div>
     </footer>
   )
 }
